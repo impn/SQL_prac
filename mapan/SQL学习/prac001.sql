623ae05333dc9e00f9ca8c87dc05321a4103c533
未闻花名 <cc_mapan@163.com>
2019-11-27 23:11:31 +0800

    2019-11-27晚上练习

--- mapan/SQL学习/prac001.sql
+++ mapan/SQL学习/prac001.sql
@@ -11,7 +11,7 @@ FROM employees
 ORDER BY `年薪` DESC;
 
 -- 描述表的结构
-DESC departments
+DESC departments;
 
 -- 按照姓名的长度显示员工的姓名和工资
 SELECT LENGTH('join');  -- length()的含义是字节长度
@@ -57,7 +57,7 @@ ORDER BY LENGTH(email) DESC,department_id ASC;
 SELECT LENGTH("join");
 SELECT LENGTH("张三丰hahaha"); -- 一个汉字占用3个字节，一个字母占用1个字节
 
-SHOW VARIABLES LIKE "%char%"
+SHOW VARIABLES LIKE "%char%";
 
 # 2.concat拼接字符串
 SELECT CONCAT(last_name,'_',first_name) AS '姓名' 
@@ -77,14 +77,156 @@ FROM employees;
 # 4.substr / substring
 
 # 从索引处开始显示 显示陆展元
-SELECT SUBSTR('李莫愁爱上了陆展元',7) out_put -- 索引从1开始
+SELECT SUBSTR('李莫愁爱上了陆展元',7) out_put; -- 索引从1开始
 
 # 截取指定索引处的字符  显示李莫愁
-SELECT SUBSTR('李莫愁爱上了陆展元',1,3) out_put -- 索引从1开始
+SELECT SUBSTR('李莫愁爱上了陆展元',1,3) out_put; -- 索引从1开始
 
 # 案例：姓名从首字母大写 ，其他字母小写 然后用_拼接，显示出来
 
 SELECT CONCAT(UPPER(SUBSTR(last_name,1,1)),'_',SUBSTR(last_name,2))
 FROM employees;
+
+# 5.instr 用于返回子串第一次出现的索引，如果找不到就返回0
+SELECT INSTR('杨不悔爱上了殷六霞','殷六霞') AS out_put;
+
+# 6.trim 去除前后的空格
+
+SELECT LENGTH(TRIM(' sdgfdsg sdgsg seg sdg ')),
+LENGTH(' sdgfdsg sdgsg seg sdg ');
+
+-- 去除字符串前后的字符 a
+SELECT TRIM('a' FROM 'aaaaa张aaa三丰aaaaaaaaaaa') AS out_put;
+
+
+# 7.lpad 最后总的字符个数是10个，功能是用指定的字符实现左填充
+
+SELECT LPAD('殷素素',10,'*') AS out_put; -- 使用*对字符串进行左填充，
+
+SELECT LPAD('美猴王',2,'*') AS out_put; -- 如果实际的字符个数大于指定的字符个数，则执行右侧截断处理
+
+# 8. rpad 类似的，rpad是右填充
+
+SELECT RPAD('殷素素',10,'*') AS out_put;
+
+SELECT RPAD('美猴王',2,'*') AS out_put;  -- 同样执行右侧截断
+
+SELECT RPAD('美猴王',12,'asddd') AS out_put; 
+
+# 9. replace 替换函数 
+
+SELECT REPLACE('我爱你，我爱你,我爱你，我爱你','爱','讨厌');
+
+# 二、数学函数
+
+# 1.round 四舍五入
+SELECT ROUND(324.235453,2);
+SELECT ROUND(2.4556);
+
+
+# 2.ceil 向上取整 该函数返回大于等于参数的最小整数
+
+SELECT CEIL(324.643);
+
+SELECT CEIL(324.00000001);
+
+SELECT CEIL(-324.643);
+
+# 3.floor 向下取整 该函数返回小于等于参数的最大整数
+
+SELECT FLOOR(324.4353);
+
+SELECT FLOOR(-324.4353);
+
+# 5.truncate 截断 保留小数点后几位，不进行四舍五入
+
+SELECT TRUNCATE('16.588',2);
+
+
+# 6. mod取余
+
+SELECT MOD(-10,3);
+
+SELECT MOD(10,3),MOD(10.0,3);
+
+# 三、日期函数
+
+# 1.now 返回当前日期和时间
+SELECT NOW();
+
+# 2. curdate 返回当前日期 不包含时间
+SELECT CURDATE();
+
+# 3.curtime 返回当前时间 不包含日期
+SELECT CURTIME();
+
+# 获取指定的部分
+SELECT CONCAT(YEAR(NOW()),"年",MONTH(NOW()),"月"); -- 拼接年份
+
+SELECT REPLACE(SUBSTR(NOW(),1,10),"-","/"); -- 字符处理
+
+SELECT MONTHNAME(NOW()) -- 获取当前月的英文名
+
+# 4.str_to_date 将字符串转换成日期
+
+SELECT STR_TO_DATE('1994/12/03','%Y-%c-%d'); -- 这样不行，日期格式 需要前后对应 比如前面是- 后面也要是-
+
+SELECT STR_TO_DATE('1994-12-03','%Y-%c-%d');
+
+SELECT STR_TO_DATE('1994/12/03','%Y/%c/%d');
+
+# 5. 查询入职日期是1992-04-03的员工信息
+
+SELECT * FROM employees WHERE hiredate = '1992-04-03';
+-- 如果输入的日期格式不规范，那么可以用str_to_date()函数
+SELECT * FROM employees WHERE hiredate = STR_TO_DATE('1992-04#3','%Y-%m#%D');
+
+# 6. data_format 将日期转换成字符串
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 

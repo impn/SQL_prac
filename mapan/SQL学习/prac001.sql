e6a628d4ee9bb4bc77bd770ef186de2447fa93ca
未闻花名 <cc_mapan@163.com>
2019-12-02 22:30:30 +0800

    标量子查询

--- mapan/SQL学习/prac001.sql
+++ mapan/SQL学习/prac001.sql
@@ -1042,6 +1042,101 @@ LEFT JOIN employees AS e
 ON e.department_id=d.department_id
 WHERE d.department_name IN('Sal','IT');
 
+# 进阶7：子查询
+
+/*
+含义：
+出现在其他语句中的select语句，称为子查询或内查询外部的查询语句，称为主查询或外查询
+
+分类：
+按子查询出现的位置：
+	select后：
+		仅仅支持标量子查询
+	from后面
+		支持表子查询
+	where或having后面：
+		标量子查询
+		列子查询
+		行子查询
+	exists后面（相关子查询）
+	表子查询
+	
+按结果集的行列数不同：
+	标量子查询（结果集只有一行一列）
+	列子查询（结果集只有一列多行）
+	行子查询（结果集有一行多列）
+	表子查询（结果集一般为多行多列）
+
+
+*/
+
+#一、where或having后面
+
+/*
+
+1、标量子查询（单行子查询）
+2、列子查询（多行子查询）
+3、行子查询（多列多行）
+
+特点：
+①子查询放在小括号内
+②子查询一般放在条件的右侧
+③标量子查询，一般搭配着单行操作符使用
+><>=<==<>
+④子查询的执行优先于主查询执行，主查询的条件用到了子查询的结果
+
+
+列子查询，一般搭配着多行操作符使用in、any/some、al1
+*/
+#1.标量子查询
+#案例1：谁的工资比Abel高？
+#①查询Abe1的工资
+
+SELECT e.last_name,e.salary
+FROM employees AS e
+WHERE e.salary >(SELECT salary FROM employees WHERE last_name='Abel')
+
+#案例2：返回job_id与141号员工相同，salary比143号员工多的员工姓名，job_id和工资
+SELECT 
+	last_name,
+	job_id,
+	salary
+FROM employees
+WHERE job_id=(
+	SELECT job_id 
+	FROM employees 
+	WHERE employee_id=141
+)AND salary>(
+	SELECT salary
+	FROM employees
+	WHERE employee_id=143
+);
+#案例3：返回公司工资最少的员工的last_name，job_id和salary
+SELECT 
+	last_name,
+	job_id,
+	salary
+FROM employees
+WHERE salary=(SELECT MIN(salary) FROM employees LIMIT 1)
+
+#案例4：查询最低工资大于50号部门最低工资的部门id和其最低工资
+SELECT 
+	department_id,
+	MIN(salary) AS min_sal
+FROM employees
+GROUP BY department_id
+HAVING min_sal>(
+	SELECT MIN(salary)
+	FROM employees
+	WHERE department_id=50
+);
+
+
+
+
+
+
+
 
 
 

3a4392149e0210655f63b8bc687dca63d395a1cf
未闻花名 <cc_mapan@163.com>
2019-12-03 08:05:03 +0800

    19-12-03早晨练习|子查询

--- mapan/SQL学习/prac001.sql
+++ mapan/SQL学习/prac001.sql
@@ -1243,13 +1243,103 @@ WHERE NOT EXISTS(
 )
 
 #练习
-/*
-1.查询和zlotkey相同部门的员工姓名和工资
-2.查询工资比公司平均工资高的员工的员工号，姓名和工资。
-3.查询各部门中工资比本部门平均工资高的员工的员工号，姓名和工资查询和姓名中包含字母u的员工在相同部门的员工的员工号和姓名5.查询在部门的location id为1700的部门工作的员工的员工号
-6.查询管理者是King的员工姓名和工资
-7.查询工资最高的员工的姓名，要求first_name和last_name显示为一列，列名为姓名。
-*/
+#1.查询Zlotkey相同部门的员工姓名和工资
+SELECT last_name,salary
+FROM employees
+WHERE department_id=(SELECT department_id
+	FROM employees
+	WHERE last_name='Zlotkey'
+);
+
+#2.查询工资比公司平均工资高的员工的员工号，姓名和工资。
+SELECT employee_id,last_name,salary
+FROM employees
+WHERE salary>(SELECT AVG(salary) FROM employees);
+
+#3.查询各部门中工资比本部门平均工资高的员工的员工号，姓名和工资
+#方法一：
+SELECT 
+	employee_id,
+	last_name,
+	department_id,
+	salary
+FROM employees AS e1
+WHERE salary>(SELECT AVG(salary)
+	FROM employees AS e2 
+	GROUP BY department_id 
+	HAVING e1.`department_id`=e2.`department_id`
+)ORDER BY employee_id,last_name;
+#方法二：
+SELECT 
+	employee_id,
+	last_name,
+	e.department_id,
+	salary
+FROM employees AS e
+JOIN(SELECT 
+	AVG(salary) AS avg_sal,
+	department_id
+	FROM employees
+	GROUP BY department_id
+)AS avg_deps
+ON e.department_id=avg_deps.department_id
+AND salary> avg_deps.avg_sal
+ORDER BY employee_id,last_name;
+
+#4.查询和姓名中包含字母u的员工在相同部门的员工的员工号和姓名
+SELECT 
+	e.employee_id,
+	e.last_name
+FROM employees AS e
+WHERE e.department_id IN(
+	SELECT
+		DISTINCT department_id
+	FROM employees
+	WHERE last_name LIKE '%u%'
+);
+
+#5.查询在部门的location_id为1700的部门工作的员工的员工号
+SELECT employee_id
+FROM employees
+WHERE department_id IN(
+	SELECT DISTINCT department_id
+	FROM departments
+	WHERE location_id=1700
+);
+
+#6.查询管理者是King的员工姓名和工资
+SELECT last_name,salary
+FROM employees
+WHERE manager_id IN(SELECT employee_id
+	FROM employees
+	WHERE last_name='K_ing'
+);
+
+# 7.查询工资最高的员工的姓名，要求first_name和last_name显示为一列，列名为姓名。
+SELECT
+	CONCAT(first_name,' ',last_name) AS 姓.名
+FROM
+	employees
+WHERE salary=(SELECT MAX(salary)FROM employees);
+
+#进阶8：分页查询★
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

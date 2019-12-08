492d698c0bfb36b68c09b8562548de06006c1e22
未闻花名 <cc_mapan@163.com>
2019-12-08 19:04:08 +0800

    2019-12-08|表结构的增删改

--- mapan/SQL学习/prac001.sql
+++ mapan/SQL学习/prac001.sql
@@ -1875,6 +1875,76 @@ CREATE TABLE author(
 
 DESC author;
 
+#2.表的修改
+/*
+alter table 表名 add|drop|modify|change colum 列名 类型;
+*/
+#①改变列名
+ALTER TABLE book CHANGE COLUMN publishDate pubdate DATETIME;
+  
+#②修改列的类型或约束
+ALTER TABLE book MODIFY COLUMN pubdate TIMESTAMP;
+
+DESC books.book
+
+#③添加新列
+ALTER TABLE author ADD COLUMN annual DOUBLE;
+
+#④删除列
+ALTER TABLE author DROP COLUMN annual;
+
+#⑤修改表名
+ALTER TABLE author RENAME TO book_author;
+
+# 3.表的删除
+
+DROP TABLE IF EXISTS author;
+SHOW TABLES;
+ 
+# 建表通用做法
+DROP TABLE IF EXISTS 表名;
+CREATE TABLE 表名();
+
+
+#4.表的复制
+INSERT INTO  books.author VALUES
+(1,'村上春树',"日本",1000),
+(2,'莫言','中国',2000),
+(3,'冯唐','中国',3999),
+(4,'金庸','中国',2444);
+SELECT * FROM author;
+DESC author;
+
+# 1.仅仅赋值表的结构
+CREATE TABLE copy1 LIKE author;
+
+SHOW TABLES;
+SELECT * FROM copy1;
+
+# 2.复制表的结构和部分内容
+CREATE TABLE copy2 SELECT * FROM author WHERE annual>1500;
+SELECT * FROM copy2;
+
+# 3.复制表的部分结构和内容
+CREATE TABLE copy3 SELECT id,au_name FROM author;
+SELECT * FROM copy3;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
